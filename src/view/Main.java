72ab9ae92f42917a8c97dd0a8e58bb371680fce9
Valber <valberdenis2016@gmail.com>
2023-12-18 23:11:52 -0300

    Final Final Final

--- src/view/Main.java
+++ src/view/Main.java
@@ -8,7 +8,6 @@ import model.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
